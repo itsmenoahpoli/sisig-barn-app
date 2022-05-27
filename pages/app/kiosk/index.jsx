e0ffc1fc064dc4e59bee6879f46d8ebbb2f9efb4
patrickpoli <patrickpolicarpio08@gmail.com>
2022-05-27 14:44:08 +0800

    kiosk app api baseurl disabled localhost url

--- pages/app/kiosk/index.jsx
+++ pages/app/kiosk/index.jsx
@@ -24,7 +24,7 @@ const KioskPage = () => {
   const [totalAmount, setTotalAmount] = React.useState(0);
 
   const getProductsByCategory = async (category) => {
-    const devURL = "http://localhost:8000/api/v1";
+    // const devURL = "http://localhost:8000/api/v1";
     const prodURL = "https://sisig-barn-app.pwnp-ws.com/public/api/v1";
 
     const axiosBaseURL = prodURL;
